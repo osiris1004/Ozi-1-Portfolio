b6abcff1d737a86f63d4de494f16bf56443dcd3d
MaesTro <kontchou05@gmail.com>
2022-10-15 20:19:11 +0200

    heading layout structure

---
+++ my-portfolio/src/components/Heading/Heading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Heading = () => {
+  return (
+    <div className={`flex py-5 font-semibold`}>
+      <div className={`border border-red-500`}>LOGO</div>
+
+      <button className={`border border-red-500 ml-auto p-2 hover:text-sky-700`}>
+        <span className={`text-sky-700`}>01.</span>About
+      </button>
+      <button className={`border border-red-500 ml-2 p-2 hover:text-sky-700`}>
+        <span className={`text-sky-700`}>02.</span>Experience
+      </button>
+      <button className={`border border-red-500 ml-2 p-2 hover:text-sky-700`}>
+        <span className={`text-sky-700`}>03.</span>Work
+      </button>
+      <button className={`border border-red-500 ml-2 p-2 hover:text-sky-700`}>
+        <span className={`text-sky-700`}>04.</span>Contact
+      </button>
+      <button className={`border border-red-500 ml-2 p-2 hover:text-sky-700 rounded-md`}>Resume</button>
+    </div>
+  );
+};
--- my-portfolio/src/pages/HomePage/HomePage.tsx
+++ my-portfolio/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
+import { Heading } from '../../components/Heading/Heading'
 
 
 const HomePage = () => {
   return (
     <div className={`container mx-auto border px-${10} border-sky-500`}>
 
-        
+        <Heading/>
     </div>
     
   )
