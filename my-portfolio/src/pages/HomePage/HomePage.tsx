faf3cc8c7173128eb751058c489f4f1a10235dd4
MaesTro <kontchou05@gmail.com>
2022-10-15 19:49:08 +0200

    seting up tailwind packages

--- my-portfolio/src/pages/HomePage/HomePage.tsx
+++ my-portfolio/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,14 @@
+
 import React from 'react'
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={`container mx-auto border px-${10} border-sky-500`}>
+
+        
+    </div>
+    
   )
 }
 
