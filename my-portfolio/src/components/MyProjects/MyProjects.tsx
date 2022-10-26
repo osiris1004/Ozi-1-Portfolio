52a2524f8ff7384ec5edb70134ac99505ba1adaa
MaesTro <kontchou05@gmail.com>
2022-10-26 15:13:49 +0200

    new algotithm for my slice show

--- my-portfolio/src/components/MyProjects/MyProjects.tsx
+++ my-portfolio/src/components/MyProjects/MyProjects.tsx
@@ -1,59 +1,51 @@
-import React from "react";
+import React, { ReactComponentElement, ReactElement, useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
-import { faSquarePersonConfined } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faSquarePersonConfined } from "@fortawesome/free-solid-svg-icons";
 export const MyProjects = () => {
 
-  const value = [1,2,3,4]
+  const slice = [<C1/>,<C2/>,<C3/>]
 
-  let currentIndex = 0 
-  const next = (n: number)=>{
-    console.log("currentIndex "+ currentIndex)
+const [currentIndex, setCurrentIndex] = useState(0)
 
-    // currentIndex < 0 ? currentIndex = value.length  : currentIndex = currentIndex+n
-    // currentIndex> value.length-1 ? currentIndex = 0 : currentIndex = currentIndex+n
+const goToNext = () =>{
+  const islastSlice = currentIndex === slice.length - 1 ;
+  const newIndex = islastSlice  ?  0:  currentIndex + 1;
+  setCurrentIndex(newIndex)
+}
 
-   if(n === -1){
-    if(currentIndex <= 0 ){
-      currentIndex = value.length 
-      currentIndex = currentIndex+n
-      console.log(value[currentIndex])
-    }else{
-      currentIndex = currentIndex+n
-      console.log(value[currentIndex])
-    }
-   }else{
+const goToPrevious = () =>{
+  const isFirstSlice = currentIndex === 0;
+  const newIndex = isFirstSlice ? slice.length - 1 :  currentIndex-1;
+  setCurrentIndex(newIndex)
 
-    if(currentIndex >= value.length-1  ){
-      currentIndex = 0
-     
-      console.log(value[currentIndex])
-    }else{
-      currentIndex = currentIndex+n
-      console.log(value[currentIndex])
-    }
-
-   }
-
-
-  
-
-  }
-
-  
-
+}
   return (
-    <div
-      className={` border-red-500 w-8/12 m-auto h-screen flex items-center`}
-    >
+    <div className={` border-red-500 w-8/12 m-auto h-screen flex items-center`}>
       <div>
-        {/* img */}
-        <div className={` border-green-500 relative `}>
+        <div className={`border border-red-500`}>
+        {slice[currentIndex]}
+        </div>
+
+        {/* my algotith */}
+        <button onClick={()=>goToNext()}>next</button>
+
+        <button onClick={()=>goToPrevious()}>prev</button>
+      </div>
+    </div>
+  );
+};
+
+const ComponentTest = () => {
+  return (
+    <div>
+      {/* img */}
+      <div className={` border-green-500 relative `}>
+        <div className={` border-red-500 absolute  inset-0 flex items-center`}>
           <div
-            className={` border-red-500 absolute  inset-0 flex items-center`}
+            className={`bg-[#092631]/[.7] h-full flex items-center text-gray-500`}
           >
-            <div className={`bg-[#092631]/[.7] h-full flex items-center text-gray-500`}>
-              <div className={`px-8`}>
+            <div className={`px-8`}>
               <p className={`fontKey text-green-500`}>Featured Project</p>
               <p className={`fontKeyHeadBoldLg text-gray-400`}>Halcyon Theme</p>
               <p className={`text mt-5`}>
@@ -62,36 +54,56 @@ export const MyProjects = () => {
                 Control, Atom Package Manager, and npm.
               </p>
               <div className={`text mt-5 fontKeySm`}>
-              <span className={`text mr-3`}>VS Code</span>
-              <span className={`text mr-3`}>Sublime Text</span>
-              <span className={`text mr-3`}>Atom</span>
-              <span className={`text mr-3`}>iTerm2</span>
-              <span className={`text mr-3`}>Hyper</span>
+                <span className={`text mr-3`}>VS Code</span>
+                <span className={`text mr-3`}>Sublime Text</span>
+                <span className={`text mr-3`}>Atom</span>
+                <span className={`text mr-3`}>iTerm2</span>
+                <span className={`text mr-3`}>Hyper</span>
               </div>
               <div className={`mt-5`}>
-                <span style={{"fontSize":30}} className={``}><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{"color":"green"}}/></span>
-                <span style={{"fontSize":30}} className={`ml-5`}><FontAwesomeIcon icon={faSquarePersonConfined} style={{"color":"green"}}/></span>
-                
-                </div>
+                <span style={{ fontSize: 30 }} className={``}>
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    style={{ color: "green" }}
+                  />
+                </span>
+                <span style={{ fontSize: 30 }} className={`ml-5`}>
+                  <FontAwesomeIcon
+                    icon={faSquarePersonConfined}
+                    style={{ color: "green" }}
+                  />
+                </span>
               </div>
-              
             </div>
           </div>
-          <img
-            alt="project img"
-            src="https://halcyon-theme.netlify.app/static/demo-30184fa83cb4ab8fb7809cf95cc8aec3.png"
-          />
         </div>
-
-        {/* my algotith */}
-        <button onClick={()=>next(1)}>
-          next
-        </button>
-
-        <button onClick={()=>next(-1)}>
-          prev
-        </button>
+        <img
+          alt="project img"
+          src="https://halcyon-theme.netlify.app/static/demo-30184fa83cb4ab8fb7809cf95cc8aec3.png"
+        />
       </div>
     </div>
   );
 };
+
+
+
+const C1 = ()=>{
+ return( <div>
+    1
+  </div>)
+}
+
+const C2 = ()=>{
+ return( <div>
+    2
+  </div>)
+}
+
+const C3 = ()=>{
+  return (<div>
+    3
+  </div>)
+}
+
+
